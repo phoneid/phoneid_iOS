ef4099ceee4a25a0730a6d24269275cb3a0e175b
Alyona <a.bachurina@gmail.com>
2015-10-09 20:02:47 +0300

    Fixed timeout for "call me"

--- Pod/Classes/ui/views/controls/VerifyCodeControl.swift
+++ Pod/Classes/ui/views/controls/VerifyCodeControl.swift
@@ -204,7 +204,7 @@ class VerifyCodeControl: PhoneIdBaseView {
     func setupHintTimer(){
         
         timer?.invalidate()
-        let fireDate = NSDate(timeIntervalSinceNow: 5)
+        let fireDate = NSDate(timeIntervalSinceNow: 30)
         timer = NSTimer(fireDate: fireDate, interval: 0, target: self, selector: "timerFired", userInfo: nil, repeats: false)
         NSRunLoop.mainRunLoop().addTimer(timer!, forMode: NSDefaultRunLoopMode)
         
