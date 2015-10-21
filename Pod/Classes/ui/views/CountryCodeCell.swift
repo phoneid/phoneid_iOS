75ff9cd6feee7f3404e18e9a0560b5ea7a3a4caf
Alyona <a.bachurina@gmail.com>
2015-10-21 17:38:25 +0300

    code polishing

--- Pod/Classes/ui/views/CountryCodeCell.swift
+++ Pod/Classes/ui/views/CountryCodeCell.swift
@@ -20,68 +20,74 @@
 
 import Foundation
 
-class CountryCodeCell:UITableViewCell{
-    
-    var model:CountryInfo?
-    var prefixLabel:UILabel!
-    var countryLabel:UILabel!
-    
-    static let height:CGFloat = 35.0
-    
+class CountryCodeCell: UITableViewCell {
+
+    var model: CountryInfo?
+    var prefixLabel: UILabel!
+    var countryLabel: UILabel!
+
+    static let height: CGFloat = 35.0
+
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        
-        prefixLabel = UILabel();
-        prefixLabel.textAlignment = NSTextAlignment.Center;
-        prefixLabel.textColor = UIColor.whiteColor();
-        prefixLabel.backgroundColor = UIColor.grayColor();
-        prefixLabel.layer.cornerRadius = 3;
-        prefixLabel.layer.masksToBounds = true;
-        prefixLabel.translatesAutoresizingMaskIntoConstraints = false
-        
-        self.contentView.addSubview(prefixLabel);
-        
-        countryLabel = UILabel();
-        countryLabel.translatesAutoresizingMaskIntoConstraints = false
-        countryLabel.font = UIFont.systemFontOfSize(18)
-        countryLabel.setContentCompressionResistancePriority(UILayoutPriorityRequired, forAxis: UILayoutConstraintAxis.Horizontal)
-        self.contentView.addSubview(countryLabel);
-        
+
+        prefixLabel = {
+            let label = UILabel()
+            label.textAlignment = NSTextAlignment.Center
+            label.textColor = UIColor.whiteColor()
+            label.backgroundColor = UIColor.grayColor()
+            label.layer.cornerRadius = 3
+            label.layer.masksToBounds = true
+            return label
+        }()
+
+        countryLabel = {
+            let label = UILabel()
+            label.font = UIFont.systemFontOfSize(18)
+            label.setContentCompressionResistancePriority(UILayoutPriorityRequired, forAxis: UILayoutConstraintAxis.Horizontal)
+            return label
+        }();
+
+        for(_, element) in [prefixLabel, countryLabel].enumerate(){
+            element.translatesAutoresizingMaskIntoConstraints = false
+            self.contentView.addSubview(element)
+        }
+
         setupLayout()
     }
-    
-    func setupLayout(){
-        var c:[NSLayoutConstraint] = []
+
+    func setupLayout() {
+        var c: [NSLayoutConstraint] = []
         let cell = self.contentView
-        
+
         c.append(NSLayoutConstraint(item: prefixLabel, attribute: .Left, relatedBy: .Equal, toItem: cell, attribute: .Left, multiplier: 1, constant: 10))
         c.append(NSLayoutConstraint(item: prefixLabel, attribute: .CenterY, relatedBy: .Equal, toItem: cell, attribute: .CenterY, multiplier: 1, constant: 0))
-        
+
         c.append(NSLayoutConstraint(item: prefixLabel, attribute: .Width, relatedBy: .Equal, toItem: nil, attribute: .Width, multiplier: 1, constant: 50))
         c.append(NSLayoutConstraint(item: prefixLabel, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .Height, multiplier: 1, constant: 30))
-        
+
         c.append(NSLayoutConstraint(item: countryLabel, attribute: .CenterY, relatedBy: .Equal, toItem: cell, attribute: .CenterY, multiplier: 1, constant: 0))
         c.append(NSLayoutConstraint(item: countryLabel, attribute: .Left, relatedBy: .Equal, toItem: prefixLabel, attribute: .Right, multiplier: 1, constant: 10))
         c.append(NSLayoutConstraint(item: countryLabel, attribute: .Right, relatedBy: .Equal, toItem: cell, attribute: .Right, multiplier: 1, constant: 0))
-        
+
         self.addConstraints(c)
-        
+
     }
-    
-    func setupWithModel(model:CountryInfo){
+
+    func setupWithModel(model: CountryInfo) {
         self.model = model
         prefixLabel.text = model.prefix
         countryLabel.text = model.name
-        
+
         self.setNeedsLayout()
     }
-    
-    func applyColorScheme(colorScheme: ColorScheme){
+
+    func applyColorScheme(colorScheme: ColorScheme) {
         self.prefixLabel.backgroundColor = colorScheme.labelPrefixBackground
         prefixLabel.textColor = colorScheme.labelPrefixText
         self.countryLabel.textColor = colorScheme.labelCountryNameText
     }
-    
+
     required init(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
