b86bc2112bfbd1010dc6da44ed792f9297f08785
Alyona Bachurina <a.bachurina@gmail.com>
2017-11-21 23:37:41 +0200

    Fixed incorrect autolayout constraint

--- Pod/Classes/ui/views/fullscreen/CountryCodePickerView.swift
+++ Pod/Classes/ui/views/fullscreen/CountryCodePickerView.swift
@@ -145,7 +145,7 @@ open class CountryCodePickerView: PhoneIdBaseFullscreenView, UITableViewDataSour
 
         var c: [NSLayoutConstraint] = []
 
-        c.append(NSLayoutConstraint(item: tableView, attribute: .left, relatedBy: .equal, toItem: self, attribute: .top, multiplier: 1, constant: 0))
+        c.append(NSLayoutConstraint(item: tableView, attribute: .left, relatedBy: .equal, toItem: self, attribute: .left, multiplier: 1, constant: 0))
         c.append(NSLayoutConstraint(item: tableView, attribute: .right, relatedBy: .equal, toItem: self, attribute: .right, multiplier: 1, constant: 0))
         c.append(NSLayoutConstraint(item: tableView, attribute: .top, relatedBy: .equal, toItem: headerBackgroundView, attribute: .bottom, multiplier: 1, constant: 0))
         c.append(NSLayoutConstraint(item: tableView, attribute: .bottom, relatedBy: .equal, toItem: self, attribute: .bottom, multiplier: 1, constant: 0))
